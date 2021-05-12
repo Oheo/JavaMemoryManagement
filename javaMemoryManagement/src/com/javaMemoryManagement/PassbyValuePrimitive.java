08602c69123b0dc1addf2fc0bd2f1a1f4e8b642d
Pankaj Addi <pankajaddi@ymail.com>
2021-05-12 14:45:06 +0530

    PassbyValuePrimitive

---
+++ javaMemoryManagement/src/com/javaMemoryManagement/PassbyValuePrimitive.java
@@ -0,0 +1,17 @@
+package com.javaMemoryManagement;
+
+
+public class PassbyValuePrimitive {
+
+	public static void main(String[] args) {
+		int localvalue =5;
+		calculate(localvalue);
+		System.out.println("localvalue" +localvalue);
+	}
+
+	private static void calculate(int calcValue) {
+		calcValue = calcValue * 100;
+		System.out.println("calcValue " +calcValue);
+	}
+}
+
